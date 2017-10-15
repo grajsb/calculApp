22c4ab12f059a9863c56a8466e3e4e0f2e773fac
Gururaj Birajdar <grajsb@gmail.com>
2017-10-14 20:02:17 -0500

    Final changes to the Simple Calculator a.k.a CalculApp :)

--- calculApp/app.js
+++ calculApp/app.js
@@ -1,11 +1,32 @@
 var app = angular.module("calculApp", []);
 app.controller("calCtrl", calCtrl);
 
-function calCtrl(){
-    var add = "+" ;
-    var sub = "-" ;
-    var mul = "*" ;
-    var div = "/" ;
+function calCtrl() {
 
-    
+    this.resultValue = 0;
+    this.operation = function (operationSymbol) {
+        this.operationSelected = operationSymbol;
+    }
+
+    this.calculate = function () {
+
+        var num1 = parseFloat(this.firstVal);
+        var num2 = parseFloat(this.secondVal);
+
+        if (this.operationSelected === "+") {
+            this.resultValue = (num1 + num2);
+        } else if (this.operationSelected === "-") {
+            this.resultValue = (num1 - num2);
+        } else if (this.operationSelected === "*") {
+            this.resultValue = (num1 * num2);
+        } else if (this.operationSelected === "/") {
+            this.resultValue = (num1 / num2);
+        }
+
+        //return resultValue;
+    }
+
+
+
+
 }
